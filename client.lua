c9482c02119066aa7ce5dd99d884c163d68dbeb5
SezerGuvener <75906653+SezerGuvener@users.noreply.github.com>
2024-12-16 08:56:04 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -82,18 +82,3 @@ RegisterNetEvent('arslan-vision:useThermalVision', function()
         end
     end)
 end)
-
--- server/main.lua
-QBCore.Functions.CreateUseableItem('nightvision', function(source)
-    local xPlayer = QBCore.Functions.GetPlayer(source)
-    if xPlayer then
-        TriggerClientEvent('arslan-vision:useNightVision', source)
-    end
-end)
-
-QBCore.Functions.CreateUseableItem('thermalvision', function(source)
-    local xPlayer = QBCore.Functions.GetPlayer(source)
-    if xPlayer then
-        TriggerClientEvent('arslan-vision:useThermalVision', source)
-    end
-end)
